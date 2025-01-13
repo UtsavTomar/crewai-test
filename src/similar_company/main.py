9664bb617555d3a92e8c0ce58e668a0a2e394216
UtsavTomar <utsav.xalt@gmail.com>
2025-01-13 13:52:33 +0530

    Update main.py

--- src/similar_company/main.py
+++ src/similar_company/main.py
@@ -10,6 +10,7 @@ from similar_company_finder_template.crew import SimilarCompanyFinderTemplateCre
 
 
 def run():
+    inputs = input("Enter comany detail")
 
     SimilarCompanyFinderTemplateCrew().crew().kickoff(inputs=inputs)
 
